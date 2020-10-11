2f49c94667df574378801e823f944be9f78e53e5
Caryn Keffer <carynkeffer@gmail.com>
2020-10-11 07:01:19 -0400

    Added Turn class and turn_test

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,2 +1,17 @@
 class Turn
+  attr_reader   :guess,
+                :card
+
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+  end
+
+  def correct?
+  end
+  
+  # def feedback
+  # end
 end
+
+# require "pry"; binding.pry
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,11 +1,37 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/turn'
+require './lib/card'
 
 class TurnTest < Minitest::Test
+
   def test_it_exists
+    skip
+    turn = Turn.new
 
     assert_instance_of Turn, turn
+  end
+
+  def test_it_has_a_guess
+    skip
+    turn = Turn.new("Juneau")
+
+    assert_equal "Juneau", turn.guess
+  end
+
+  def test_it_has_a_card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal card, turn.card
+  end
+
+  def test_if_guess_is_correct
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal "Juneau", turn.guess
+
   end
 
 end
